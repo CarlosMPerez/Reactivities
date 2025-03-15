52538fb287cd0e2f963384fb0828337681feddfd
Carlos M. Pérez <6448714+CarlosMPerez@users.noreply.github.com>
2025-03-15 17:40:44 +0100

    Added MobX to packages. Store counter for demo purposes

--- client/src/app/layout/NavBar.tsx
+++ client/src/app/layout/NavBar.tsx
@@ -1,43 +1,48 @@
-import { Group } from '@mui/icons-material';
-import { Box, AppBar, Toolbar, 
-    Container, MenuItem, 
-    Typography } from '@mui/material';
-import { NavLink } from 'react-router';
-import MenuItemLink from '../shared/components/MenuItemLink';
+import { Group } from "@mui/icons-material";
+import {
+  Box,
+  AppBar,
+  Toolbar,
+  Container,
+  MenuItem,
+  Typography,
+} from "@mui/material";
+import { NavLink } from "react-router";
+import MenuItemLink from "../shared/components/MenuItemLink";
 
 export default function NavBar() {
-
   return (
     <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="static" sx={{ 
-        backgroundImage: 'linear-gradient(1354deg, #182a73 0%, #218aae 69%, #20a7ac 89%)'
-    }}>
-        <Container maxWidth='xl'>
-            <Toolbar sx={{display: 'flex', justifyContent: 'space-between'}}>
-                <Box>
-                    <MenuItem 
-                        component={NavLink} to='/' 
-                        sx={{display: 'flex', gap: 2}}>
-                            <Group fontSize="large" />
-                            <Typography variant="h4" 
-                                fontWeight="bold">Reactivities
-                            </Typography>
-                    </MenuItem>
-                </Box>
-                <Box sx={{display: "flex"}}>
-                    <MenuItemLink to='/activities' >
-                        Activities
-                    </MenuItemLink>
-                    <MenuItemLink to="/createActivity" >
-                        Create Activity
-                    </MenuItemLink>
-                </Box>
-                <MenuItem>
-                    User Menu
-                </MenuItem>
-            </Toolbar>
+      <AppBar
+        position="static"
+        sx={{
+          backgroundImage:
+            "linear-gradient(1354deg, #182a73 0%, #218aae 69%, #20a7ac 89%)",
+        }}
+      >
+        <Container maxWidth="xl">
+          <Toolbar sx={{ display: "flex", justifyContent: "space-between" }}>
+            <Box>
+              <MenuItem
+                component={NavLink}
+                to="/"
+                sx={{ display: "flex", gap: 2 }}
+              >
+                <Group fontSize="large" />
+                <Typography variant="h4" fontWeight="bold">
+                  Reactivities
+                </Typography>
+              </MenuItem>
+            </Box>
+            <Box sx={{ display: "flex" }}>
+              <MenuItemLink to="/activities">Activities</MenuItemLink>
+              <MenuItemLink to="/createActivity">Create Activity</MenuItemLink>
+              <MenuItemLink to="/counter">Counter</MenuItemLink>
+            </Box>
+            <MenuItem>User Menu</MenuItem>
+          </Toolbar>
         </Container>
-    </AppBar>
-  </Box>
-  )
+      </AppBar>
+    </Box>
+  );
 }
--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,22 +1,24 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import './app/layout/styles.css'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
-import { RouterProvider } from 'react-router';
-import { router } from './app/router/Routes.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+import "./app/layout/styles.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { RouterProvider } from "react-router";
+import { router } from "./app/router/Routes.tsx";
+import { StoreContext, store } from "./lib/stores/store.ts";
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
+    <StoreContext.Provider value={store} />
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools initialIsOpen={false} />
       <RouterProvider router={router} />
     </QueryClientProvider>
-  </StrictMode>,
-)
+  </StrictMode>
+);
